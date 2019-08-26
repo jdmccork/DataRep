cb6471d4e454eae20aef77720c33ead213136902
jdmccork <15jmccorkindale@wakatipu.school.nz>
2019-08-27 09:54:33 +1200

    Completed binary arrow compatability

--- app.js
+++ app.js
@@ -23,36 +23,48 @@ function decConvert(){
 }
 
 function binConvert(){
-  var bin = parseFloat(binaryInput.value);
-  var re = /-?[0-1]+$/;
   if (binaryInput.value==''){
-    decimalInput.value = '';
+    deciamlInput.value = '';
     octalInput.value = '';
     hexadecimalInput.value = '';
     return;
   }
-  if(!re.test(binaryInput.value)) {
-    alert("Error: Binary inputs can only contain 1 or 0");
-    binaryInput.value = bin;
-    binaryInput.focus();
-    decimalInput.value = '';
-    octalInput.value = '';
-    hexadecimalInput.value = '';
-    return false;
+  while(binaryInput.value.includes("2") || binaryInput.value.includes("9")) {
+    if(binaryInput.value==binOld+1) {
+      while(binaryInput.value.includes("2") || binaryInput.value.includes("9")){
+        if (octalInput.value<0){
+          binaryInput.value=parseInt(binaryInput.value)+8*Math.pow(10,binaryInput.value.length-(binaryInput.value.indexOf('9')+1))
+        }else{
+        binaryInput.value=parseInt(binaryInput.value)+8*Math.pow(10,binaryInput.value.length-(binaryInput.value.indexOf('2')+1))
+        }
+      }
+    } else if(binaryInput.value==binOld-1) {
+      while(binaryInput.value.includes("2") || binaryInput.value.includes("9")){
+        if(octalInput.value<0){
+          binaryInput.value=parseInt(binaryInput.value)-8*Math.pow(10,binaryInput.value.length-(binaryInput.value.indexOf('2')+1))
+        }else{
+          binaryInput.value=parseInt(binaryInput.value)-8*Math.pow(10,binaryInput.value.length-(binaryInput.value.indexOf('9')+1)) //2*position of incorrect value 10^position
+        }
+      }
+    }else {
+      alert("Error: Binary inputs can only contain 1 or 0");
+      binaryInput.value = bin;
+      binaryInput.focus();
+      decimalInput.value = '';
+      octalInput.value = '';
+      hexadecimalInput.value = '';
+      return false;
+    }
   }
+  var bin = parseFloat(binaryInput.value);
   var dec = parseInt(bin,2);
   var oct = dec.toString(8);
   var hex = dec.toString(16);
   decimalInput.value = dec;
   octalInput.value = oct;
   hexadecimalInput.value = hex;
+  binOld=bin
 }
-
-
-
-
-
-
 
 
 function octConvert(){
@@ -96,15 +108,6 @@ function octConvert(){
   octOld = oct
 }
 
-
-
-
-
-
-
-
-
-
 function hexConvert(){
   var hex = hexadecimalInput.value;
   var re = /-?[0-9a-fA-F]+$/;
@@ -134,6 +137,10 @@ function main(){
   hexadecimalInput.addEventListener('input',hexConvert);
 }
 
+//var testog="9281"
+//if(testog.some(["5","4","2"])){
+//  console.log('yes')
+//}
 main();
 
 
@@ -150,9 +157,6 @@ function octIn(){
   const dec = parseInt(temp,8);
   conAll(dec);
 }
-
-
-
 
 
 function conAll(dec){
